4de6e7646b7fa72c49a502dae6744541802e2ae6
haricolor <matuzawahibiki@gmail.com>
2017-09-04 16:04:21 +0900

    no message

---
+++ BlockBreake/Assets/Script/GameWindowSetting.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameWindowSetting : MonoBehaviour {
+
+    [RuntimeInitializeOnLoadMethod]
+    static void OnRuntimeMethodLoad()
+    {
+        //Screen.SetResolution(926, 520, false, 60);
+
+    }
+
+    // Use this for initialization
+    //void Start () {
+		
+	//}
+	
+	// Update is called once per frame
+	//void Update () {
+		
+	//}
+}
